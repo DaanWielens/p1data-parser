c14b7c1fd2619c653422fae7fbb752bacc3e1d6b
Daan Wielens <github@daanwielens.com>
2019-09-06 20:39:33 +0200

    Bugfixes
    Bugfixes and error handling for faulty ASCII data streams

--- p1logger.py
+++ p1logger.py
@@ -2,9 +2,9 @@
 """
 Python script to log p1 data in a .csv file
 
-v0.1
+v0.2
 Daan Wielens
-2019-09-04
+2019-09-06
 
 Based on: http://domoticx.com/p1-poort-slimme-meter-telegram-uitlezen-met-python/
 """
@@ -41,42 +41,57 @@ ser.close()
 #%% Main code
 
 while True:
-  ser.open()
-  checksum_found = False
+    try:
+      ser.open()
+      checksum_found = False
 
-  while not checksum_found:
-    telegram_line = ser.readline()
-    telegram_line = telegram_line.decode('ascii').strip()
+      while not checksum_found:
+        telegram_line = ser.readline()
+        telegram_line = telegram_line.decode('ascii').strip()
 
-    # Check if data matches preset variables (https://github.com/DaanWielens/p1data-parser/blob/master/parser.json)
-    if '1-0:1.7.0' in telegram_line: #1-0:1.7.0 = Actual use in kW
-      # 1-0:1.7.0(0000.54*kW)
-      kw = telegram_line.split('(')[1].split('*')[0]
-      watt = float(kw) * 1000
+        # Check if data matches preset variables (https://github.com/DaanWielens/p1data-parser/blob/master/parser.json)
+        if '1-0:1.7.0' in telegram_line: #1-0:1.7.0 = Actual use in kW
+          # 1-0:1.7.0(0000.54*kW)
+          kw = telegram_line.split('(')[1].split('*')[0]
+          watt = float(kw) * 1000
 
-    if '0-1:24.2.1' in telegram_line: #0-1:24.2.1 = Gas in m3
-      gmcubed = telegram_line.split('(')[2].split('*')[0]
+        if '0-1:24.2.1' in telegram_line: #0-1:24.2.1 = Gas in m3
+          gmcubed = telegram_line.split('(')[2].split('*')[0]
 
-    # Check whether end of telegram was received
-    if '!' in telegram_line:
-      checksum_found = True
+        # Check whether end of telegram was received
+        if '!' in telegram_line:
+          checksum_found = True
 
-  ser.close()
+      ser.close()
 
-  # Write data to file
-  ts = int(datetime.datetime.now().timestamp())
-  data_str = ', '.join([str(ts), str(watt), str(gmcubed)]) + '\n'
+      # Write data to file
+      ts = int(datetime.datetime.now().timestamp())
+      data_str = ', '.join([str(ts), str(watt), str(gmcubed)]) + '\n'
 
-  cur_day = datetime.datetime.now().strftime('%Y-%m-%d')
-  filename = 'p1data/' + cur_day + '_p1data.csv'
+      cur_day = datetime.datetime.now().strftime('%Y-%m-%d')
+      filename = 'p1data/' + cur_day + '_p1data.csv'
 
-  if os.path.isfile(filename):
-      # Write data
-      with open(filename, 'a') as file:
-          file.write(data_str)
+      if os.path.isfile(filename):
+          # Write data
+          with open(filename, 'a') as file:
+              file.write(data_str)
 
-  else:
-      # Write header
-      with open(filename, 'w') as file:
-          file.write('Time (epoch), Power (W), Gas (m3)\n')
-          file.write(data_str)
+      else:
+          # Write header
+          with open(filename, 'w') as file:
+              file.write('Time (epoch), Power (W), Gas (m3)\n')
+              file.write(data_str)
+
+    except Exception as e:
+        ser.close()
+        logfile = '/home/pi/scripts/p1errorlog'
+        e = str(e)
+        if os.path.isfile(logfile):
+            with open(logfile, 'a') as file:
+                file.write(e)
+                file.write('\n')
+        else:
+            with open(logfile, 'w') as file:
+                file.write(e)
+                file.write('\n')
+
